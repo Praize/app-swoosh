1d961717cee803a4fadcb835d226f453fc3dd240
= <=>
2020-04-01 11:34:38 +0200

    segue section and unwindSegue

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var swoosh: UIImageView!
-    
     @IBOutlet weak var bgImg: UIImageView!
     
     override func viewDidLoad() {
@@ -22,6 +21,10 @@ class ViewController: UIViewController {
 //        swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
 //        
 //        bgImg.frame = view.frame;
+    }
+    //for back button
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
     }
 
 
